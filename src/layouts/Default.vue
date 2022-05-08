87774d15dcbb6465e25fb60910e91f7ff22f6944
Ricardo Morin <morinricardo@gmail.com>
2022-05-08 14:10:37 -0700

    Defer tab data loading

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -30,7 +30,7 @@
       <footer class="footer">
         <div class="has-text-centered">
           <div class="footer-text" style="color: white;">
-          Este sitio es solamente un prototipo. v0.0.1.
+          Este sitio es solamente un prototipo. v0.0.2.
           </div>
         </div>
       </footer>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,8 +6,8 @@
       </h1>
     </template>
 
-    <b-tabs >
-      <b-tab-item label="Registro de presencias">
+    <b-tabs @input="tabChanged" v-model="activeTab">
+      <b-tab-item value="occurrences" active label="Registro de presencias">
         <h4 class="title is-4 has-text-centered">Registros de presencia de especies en Venezuela</h4>
         <b-field>
           <b-autocomplete style="width: 30%;"
@@ -32,7 +32,7 @@
           <b-button type="is-primary" @click="loadGbifOccurrences()">Aplicar</b-button>
           <b-button icon-right="trash" @click="name=''; tags=[]; loadGbifOccurrences(1)"/>
         </b-field>
-        <b-table style="cursor: pointer;"
+        <b-table
           :data='gbifOccurrencesData'
           :loading='loading'
           hoverable
@@ -40,11 +40,10 @@
           :pagination-simple='true'
           backend-pagination
           :total='totalGbifOccurrences'
-          @click='gbifOccurrencesRowClick'
           @page-change='gbifOccurrencesOnPageChange'
         >
           <b-table-column width="30%" field="scientificName" label="Nombre científico" v-slot="props">
-            {{ props.row.scientificName }}
+            <a :href="'https://gbif.org/es/occurrence/' + props.row.gbifID">{{ props.row.scientificName }}</a>
           </b-table-column>
           <b-table-column field="year" label="Año" v-slot="props">
             {{ props.row.year }}
@@ -60,7 +59,7 @@
           </b-table-column>
         </b-table>
       </b-tab-item>
-      <b-tab-item label="Juegos de datos">
+      <b-tab-item value="datasets" label="Juegos de datos">
         <h4 class="title is-4 has-text-centered">Juegos de datos publicados en Venezuela</h4>
         <b-table style="cursor: pointer;"
           :data='gbifDatasetsData'
@@ -70,11 +69,10 @@
           :pagination-simple='true'
           backend-pagination
           :total='totalGbifDatasets'
-          @click='gbifDatasetsRowClick'
           @page-change='gbifDatasetsOnPageChange'
         >
           <b-table-column width="50%" field="title" label="Título" v-slot="props">
-            {{ props.row.title }}
+            <a :href="'https://gbif.org/es/dataset/' + props.row.key">{{ props.row.title }}</a>
           </b-table-column>
           <b-table-column field="publishingOrganizationTitle" label="Organización" v-slot="props">
             {{ props.row.publishingOrganizationTitle }}
@@ -111,6 +109,7 @@ export default {
     return {
       gbifOccurrencesData: [],
       gbifDatasetsData: [],
+      activeTab: 'occurrences',
       searchAutoData: [],
       name: '',
       totalGbifOccurrences: 0,
@@ -137,26 +136,20 @@ export default {
   },
   mounted() {
     this.loadGbifOccurrences()
-    this.loadGbifDatasets()
-    /*getGbifOccurrences().then((result) => {
-      console.log(result)
-    }) */
+    //this.loadGbifDatasets()
   },
   methods: {
     loadGbifOccurrences(page) {
-      //this.loading = true
+      this.loading = true
       getGbifOccurrences((page-1)*20, this.name, this.tags).then((result) => {
         this.gbifOccurrencesData = result.data.results
         this.totalGbifOccurrences = result.data.count
-        //this.loading = false
+        this.loading = false
       })
     },
     gbifOccurrencesOnPageChange(page) {
       this.gbifOccurrencesPage = page
       this.loadGbifOccurrences(page)
-    },
-    gbifOccurrencesRowClick(row) {
-      window.location.href=('https://gbif.org/es/occurrence/' + row.gbifID)
     },
     loadGbifDatasets(page) {
       this.loading = true
@@ -177,10 +170,6 @@ export default {
       this.gbifDatasetsPage = page
       this.loadGbifDatasets(page)
     },
-    gbifDatasetsRowClick(row) {
-      window.location.href=('https://gbif.org/es/dataset/' + row.key)
-      //console.log(row)
-    },
     getSpeciesSuggestions(name) {
       getSpeciesSuggestions(name).then((result) => {
         this.searchAutoData = result.data
@@ -191,6 +180,9 @@ export default {
         return (geo[i].boundingBox.minLongitude > -59.57 || geo[i].boundingBox.maxLongitude < -73.036 || geo[i].boundingBox.maxLatitude < 0.72 || geo[i].boundingBox.minLatitude > 12.17)
       }
       return false
+    },
+    tabChanged() {
+      if (this.activeTab === 'datasets' && this.gbifDatasetsData.length === 0) this.loadGbifDatasets()
     }
   }
 }
